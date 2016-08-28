740043e66cf467b9c98914db8cb1668e24478073
Matteo Scurati <matteo.scurati@gmail.com>
2016-08-28 10:24:03 +0200

    Documents index page

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,9 +2,9 @@ class Author < ApplicationRecord
   has_many :articles
 
   scope :ordered_by_articles, -> {
-    select("authors.id, authors.name, count(articles.id) as articles_count")
+    select("authors.id, authors.name, count(articles.id)")
     .joins(:articles)
     .group("authors.id")
-    .order("articles_count DESC")
+    .order("count(articles.id) DESC")
   }
 end
